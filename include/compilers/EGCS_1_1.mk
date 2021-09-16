1e235976b077b54efefa2fb131b63c9dd288f880
Jaison Lee <jsahnlee@gmail.com>
2021-09-16 14:41:59 +0900

    delete unused mks

--- include/compilers/EGCS_1_1.mk
+++
@@ -1,1 +0,0 @@
-include SoftRelTools/compilers/EGCS.mk
--- include/compilers/GCC_3_0.mk
+++
@@ -1,4 +0,0 @@
-include SoftRelTools/compilers/GCC.mk
-
--include SRT_$(SRT_PROJECT)/special/compilers/GCC_3_0.mk
--include SRT_SITE/special/compilers/GCC_3_0.mk
